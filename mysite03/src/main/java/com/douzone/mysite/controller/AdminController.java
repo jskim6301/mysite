a642f597690e52b1a4af8c0c747a6737208ed7a3
BIT <BIT@DESKTOP-HU43P3A>
2020-03-16 21:41:15 +0900

    file upload 03-16

--- mysite03/src/main/java/com/douzone/mysite/controller/AdminController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/AdminController.java
@@ -1,18 +1,42 @@
 package com.douzone.mysite.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.douzone.mysite.service.AdminService;
+import com.douzone.mysite.vo.AdminVO;
 import com.douzone.security.Auth;
 
 @Auth("ADMIN")
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-	
+	@Autowired
+	private AdminService adminService;
 	
 	@RequestMapping("")
-	public String main() {
+	public String main(Model model) {
+		AdminVO adminVO = adminService.getContents();
+		model.addAttribute("adminVO",adminVO);
+		return "admin/main";
+	}
+	@RequestMapping(value="/main/update",method=RequestMethod.POST)
+	public String main(
+			@ModelAttribute AdminVO adminVO,Model model) {	
+		System.out.println("adminVO.getMultipartFile() "+adminVO.getMultipartFile());
+		System.out.println("adminVO.getTitle() "+adminVO.getTitle());
+		System.out.println("adminVO.getWelcomeMessage() "+adminVO.getWelcomeMessage());
+		System.out.println("adminVO.getDescription() "+adminVO.getDescription());
+		
+		String url = adminService.restore(adminVO.getMultipartFile());
+		adminVO.setProfile(url);
+		adminService.modifyContents(adminVO);
+		
+
 		return "admin/main";
 	}
 	
--- mysite03/src/main/java/com/douzone/mysite/controller/MainController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/MainController.java
@@ -1,13 +1,24 @@
 package com.douzone.mysite.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.douzone.mysite.service.AdminService;
+import com.douzone.mysite.vo.AdminVO;
 
 @Controller
 public class MainController {
+
+	@Autowired
+	private AdminService adminService;
 	
 	@RequestMapping({"","/main"})
-	public String index() {
+	public String index(Model model) {
+		
+		AdminVO adminVO = adminService.getContents();
+		model.addAttribute("adminVO",adminVO);
 		return "main/index";
 	}
 	
