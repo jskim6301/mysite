ff4e74dcd86128d33213f3d9b9262e0ce0ba487a
BIT <BIT@DESKTOP-HU43P3A>
2020-03-27 16:08:56 +0900

    mysite06 설정 추가

---
+++ mysite06/src/main/java/com/douzone/mysite/controller/BoardController.java
@@ -0,0 +1,196 @@
+package com.douzone.mysite.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.douzone.mysite.service.BoardService;
+import com.douzone.mysite.vo.BoardVO;
+import com.douzone.mysite.vo.UserVO;
+import com.douzone.security.Auth;
+import com.douzone.security.AuthUser;
+import com.douzone.web.util.WebUtil;
+
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+	
+	@Autowired
+	private BoardService boardService;
+	
+	@RequestMapping(value="")
+	public String list(
+			@RequestParam(value="p",required=true,defaultValue="1") int currentPage,
+			@RequestParam(value="kwd",required=true,defaultValue="") String keyword,
+			Model model) {
+		
+		Map<String,Object> map = boardService.getContentsList(currentPage,keyword);
+		model.addAttribute("map",map);
+		
+//		model.addAllAttributes(map);//jsp화면에서 이름(value)으로 바로 접근
+		return "board/list";
+	}
+	
+	@RequestMapping(value= "/view/{no}",method=RequestMethod.GET)
+	public String view(@PathVariable("no") Long no,Model model) {
+		BoardVO boardVO = boardService.getContents(no);
+		model.addAttribute("boardVO",boardVO);
+		return "board/view";
+	}
+
+	
+	
+	@Auth
+	@RequestMapping(value="/write",method=RequestMethod.GET)
+//	public String write(HttpSession session) {
+	public String write() {	
+/*
+		//////////////////////////////접근제어//////////////////////////
+		UserVO authUser = (UserVO)session.getAttribute("authUser");
+		if(authUser == null) {
+			return "redirect:/";
+		}
+		//////////////////////////////////////////////////////////////		
+*/
+		return "board/write";
+	}
+	
+	@Auth
+	@RequestMapping(value="/write",method=RequestMethod.POST)
+	public String write(
+//			HttpSession session,
+			@AuthUser UserVO authUser,
+			@ModelAttribute BoardVO boardVO,
+			@RequestParam(value="p",required=true,defaultValue="1") Integer page,
+			@RequestParam(value="kwd", required=true,defaultValue="") String keyword) {
+/*
+		//////////////////////////////접근제어//////////////////////////
+		UserVO authUser = (UserVO)session.getAttribute("authUser");
+		if(authUser == null) {
+			return "redirect:/";
+		}
+		//////////////////////////////////////////////////////////////
+*/
+//		UserVO authUser = (UserVO)session.getAttribute("authUser");
+		boardVO.setUserNo(authUser.getNo()); //로그인 한 사람이 답글을 달 수 있도록 한다.
+		boardService.addContents(boardVO); //처음 넣는 것
+		
+		return "redirect:/board?p="+page+"&kwd="+WebUtil.encodeURL(keyword,"UTF-8");
+	}
+	
+	@Auth
+	@RequestMapping(value="/reply/{no}")
+	public String reply(
+//			HttpSession session,
+			@AuthUser UserVO authUser,
+			@PathVariable("no") Long no,
+			Model model) {
+/*
+		//////////////////////////////접근제어//////////////////////////
+		UserVO authUser = (UserVO)session.getAttribute("authUser");
+		if(authUser == null) {
+			return "redirect:/";
+		}
+		//////////////////////////////////////////////////////////////
+*/
+		BoardVO boardVO = boardService.getContents(no);
+		boardVO.setoNo(boardVO.getoNo()+1);
+		boardVO.setDepth(boardVO.getDepth()+1);
+		model.addAttribute("boardVO",boardVO);
+		
+		return "board/reply";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@Auth
+	@RequestMapping( "/delete/{no}" )
+	public String delete(
+//		HttpSession session,
+		@AuthUser UserVO authUser,
+		@PathVariable( "no" ) Long boardNo,
+		@RequestParam( value="p", required=true, defaultValue="1") Integer page,
+		@RequestParam( value="kwd", required=true, defaultValue="") String keyword ) {
+/*
+		/////////////////////////////접근제어////////////////////////
+		UserVO authUser = (UserVO)session.getAttribute("authUser");
+		if(authUser == null) {
+			return "redirect:/";
+		}
+		///////////////////////////////////////////////////////////
+*/
+		
+//		UserVO authUser = (UserVO)session.getAttribute("authUser");
+		boardService.deleteContents( boardNo, authUser.getNo() );
+		return "redirect:/board?p=" + page + "&kwd=" + WebUtil.encodeURL( keyword, "UTF-8" );
+	}
+	
+	@Auth
+	@RequestMapping( value="/modify/{no}" )	
+	public String modify(
+//		HttpSession session,
+		@AuthUser UserVO authUser,
+		@PathVariable( "no" ) Long no,
+		Model model) {
+/*
+		/////////////////////////////접근제어////////////////////////
+		UserVO authUser = (UserVO)session.getAttribute("authUser");
+		if(authUser == null) {
+			return "redirect:/";
+		}
+		///////////////////////////////////////////////////////////
+*/
+		
+//		UserVO authUser = (UserVO)session.getAttribute("authUser");
+		BoardVO boardVO = boardService.getContents(no, authUser.getNo() );
+		model.addAttribute( "boardVO", boardVO );
+		return "board/modify";
+	}
+	
+	@Auth
+	@RequestMapping( value="/modify", method=RequestMethod.POST )	
+	public String modify(
+//		HttpSession session,	
+		@AuthUser UserVO authUser,
+		@ModelAttribute BoardVO boardVO,
+		@RequestParam( value="p", required=true, defaultValue="1") Integer page,
+		@RequestParam( value="kwd", required=true, defaultValue="") String keyword ) {
+/*		
+		/////////////////////////////접근제어////////////////////////
+		UserVO authUser = (UserVO)session.getAttribute("authUser");
+		if(authUser == null) {
+			return "redirect:/";
+		}
+		///////////////////////////////////////////////////////////
+*/		
+//		UserVO authUser = (UserVO)session.getAttribute("authUser");
+		boardVO.setUserNo( authUser.getNo() );
+		boardService.modifyContents( boardVO );
+		return "redirect:/board/view/" + boardVO.getNo() + 
+				"?p=" + page + 
+				"&kwd=" + WebUtil.encodeURL( keyword, "UTF-8" );
+	}	
+	
+	
+	
+	
+}
+
---
+++ mysite06/src/main/java/com/douzone/mysite/service/BoardService.java
@@ -0,0 +1,106 @@
+package com.douzone.mysite.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.douzone.mysite.repository.BoardRepository;
+import com.douzone.mysite.vo.BoardVO;
+
+@Service
+public class BoardService {
+	
+	private static final int postNum = 10;
+	private static final int pageNum_cnt = 10; 		//한번에 표시할 페이징 번호의 갯수
+	
+	@Autowired
+	private BoardRepository boardRepository;
+	
+
+
+	public Map<String,Object> getContentsList(int currentPage,String keyword) { //초기값 page=1(int), keyword = ""
+		int count = boardRepository.getTotalCount(keyword);
+		
+		int displayPost = (currentPage -1) * postNum;
+				
+		int endPageNum = (int)(Math.ceil((double)currentPage/(double)pageNum_cnt) * pageNum_cnt);// currentPage 가 156일 경우 endPageNum 은 160
+		int startPageNum = endPageNum - (pageNum_cnt-1);//startPageNum은 151~160
+		
+		int endPageNum_tmp = (int)(Math.ceil((double)count/(double)pageNum_cnt));
+		if(endPageNum > endPageNum_tmp) {
+			endPageNum = endPageNum_tmp;
+		}
+		boolean prev = startPageNum == 1 ? false : true;//시작 페이지 번호가 1일 때를 제외하곤 무조건 출력
+		
+		boolean next = endPageNum * pageNum_cnt >= count ? false : true;//마지막 페이지 번호가 총 게시물 갯수보다 작다면, 다음 구간이 있다는 의미이므로 출력
+		
+		List<BoardVO> list = boardRepository.findAll(displayPost,postNum,keyword);
+		Map<String,Object> map = new HashMap<String,Object>();
+		
+		map.put("list",list);
+		
+		map.put("prev",prev);
+		map.put("next",next);
+		
+		map.put("count",count);
+		map.put("currentPage",currentPage);
+		map.put("displayPost",displayPost);
+		
+		map.put("startPageNum",startPageNum);
+		map.put("endPageNum",endPageNum);
+		
+		map.put("keyword",keyword);
+		
+		return map;
+	}
+
+
+
+	public BoardVO getContents(Long no) {
+		BoardVO boardVO = boardRepository.findByNo(no);
+		
+		if(boardVO != null) {
+			boardRepository.updateHit(no);
+		}
+		
+		return boardVO;
+	}
+
+	public BoardVO getContents( Long no, Long userNo ) {
+		BoardVO boardVO = boardRepository.findByNoAndUserNo( no, userNo );
+		return boardVO;
+	}
+
+
+	public boolean addContents(BoardVO boardVO) {
+		if(boardVO.getgNo() != null) { //그룹넘버가 있다는 말은 
+			increaseGroupOrderNo(boardVO);
+		}
+		return boardRepository.insert(boardVO) == 1;		
+	}
+
+
+
+	public boolean increaseGroupOrderNo(BoardVO boardVO) {
+		return boardRepository.updateSequece(boardVO.getgNo(),boardVO.getoNo()) > 0; 
+		
+	}
+
+
+
+	public boolean deleteContents(Long boardNo, Long userNo) {
+		int count = boardRepository.delete(boardNo, userNo);
+		return count == 1;		
+	}
+
+
+
+	public boolean modifyContents( BoardVO boardVO ) {
+		int count = boardRepository.update( boardVO );
+		return count == 1;
+	}
+
+}
